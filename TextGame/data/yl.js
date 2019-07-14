9b69f2718d8a87375adf7ea3a534f09b5969cb89
yanglei26 <yanglei26@meituan.com>
2019-07-15 00:45:31 +0800

    feat: 添加迅奇大数据打包代码

--- TextGame/data/yl.js
+++ TextGame/data/yl.js
@@ -3,8 +3,16 @@
  */
 var userInfo = {
     name: '瘦子',
-    level: 1,
-    zdl: 1,
-    exp: 0
+    level: 14,
+    exp: 1584250,
+    zdl: 33,
+    fabao: {
+        ['普通神器礼服']: {
+            description: '经验加成30%'
+        },
+        ['蓝色冒险者徽章']: {
+            description: '战斗力+2'
+        }
+    }
 };
 window.onUserInfo(userInfo);
